ce6b9f1e28898fee1187b932fafdf299e6893bbd
John Winters <jhnwin@amazon.com>
2019-05-28 21:04:37 -0700

    add image and readme

--- code-challenges401/src/main/java/code/challenges401/ArrayShift.java
+++ code-challenges401/src/main/java/code/challenges401/ArrayShift.java
@@ -1,18 +1,20 @@
 package code.challenges401;
 
-import java.util.Arrays;
 import java.lang.Math;
 
 public class ArrayShift {
+    // This function takes in an array of int and an int and
+    // puts that int into the middle of the Array, It returns the new array
     public int[] insertShiftArray(int[] inputArray, int inputNumber){
-        //Setup variables
+        // Setup variables
         int arrayLength = inputArray.length+1;
         int[] outputArray = new int[arrayLength];
         int outputIncrementer = 0;
         int inputIncrementer = 0;
         int middle = (int) Math.floor(arrayLength / 2);
-        //Loop through and copy
+        // Loop through and copy
         while(outputIncrementer < arrayLength){
+            //I
             if(outputIncrementer == middle) {
                 outputArray[outputIncrementer] = inputNumber;
             }else{
@@ -21,8 +23,6 @@ public class ArrayShift {
             }
             outputIncrementer++;
         }
-        System.out.println(Arrays.toString(inputArray));
-        System.out.println(Arrays.toString(outputArray));
         return outputArray;
     }
 }
