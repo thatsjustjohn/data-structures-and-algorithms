89fde17a0f500052e140fe8a0d72ac04e9f3ce04
John Winters <jhnwin@amazon.com>
2019-05-28 20:13:46 -0700

    setup tests and get stuff printing / failing

---
+++ code-challenges401/src/main/java/code/challenges401/ArrayShift.java
@@ -0,0 +1,10 @@
+package code.challenges401;
+
+import java.util.Arrays;
+
+public class ArrayShift {
+    public int[] insertShiftArray(int[] inputArray, int inputNumber){
+        System.out.println(Arrays.toString(inputArray));
+        return inputArray;
+    }
+}
---
+++ code-challenges401/src/test/java/code/challenges401/ArrayShiftTest.java
@@ -0,0 +1,23 @@
+package code.challenges401;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ArrayShiftTest {
+    @Test
+    public void arrayShift_test_output_odd() {
+        ArrayShift classUnderTest = new ArrayShift();
+        assertArrayEquals(new int[] {2,4,5,6,8}, classUnderTest.insertShiftArray(new int[]{2,4,6,8}, 5));
+    }
+    @Test
+    public void arrayShift_test_output_even() {
+        ArrayShift classUnderTest = new ArrayShift();
+        assertArrayEquals(new int[] {4,8,15,16,23,42}, classUnderTest.insertShiftArray(new int[]{4,8,15,23,42}, 16));
+    }
+    @Test
+    public void arrayShift_test_output_true() {
+        ArrayShift classUnderTest = new ArrayShift();
+        assertArrayEquals(new int[] {4,8,15,16,23,42}, classUnderTest.insertShiftArray(new int[]{4,8,15,16,23,42}, 16));
+    }
+}
