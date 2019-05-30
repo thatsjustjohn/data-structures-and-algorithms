f1dce5a0c54346420cd9fbab5dbd07d7369a4b6c
John Winters <jhnwin@amazon.com>
2019-05-30 11:49:03 -0700

    wit

--- code-challenges401/src/main/java/code/challenges401/BinarySearch.java
+++ code-challenges401/src/main/java/code/challenges401/BinarySearch.java
@@ -1,4 +1,19 @@
 package code.challenges401;
 
 public class BinarySearch {
+    public int binarySearch(int[] inputArray, int searchKey){
+        // Setup variables
+        int high = inputArray.length - 1;
+        int low = 0;
+        int mid = 0;
+        // Loop through array and find item
+        while(low <= high){
+            mid = high == low ? high : (high + low)/2;
+            if(inputArray[mid] == searchKey) return mid;
+            if(inputArray[mid] > searchKey) high = mid - 1;
+            else low = mid + 1;
+        }
+        // Return -1 if not found
+        return -1;
+    }
 }
