11e30b134d8cb42c60cc33b5d5a491da2504860c
John Winters <jhnwin@amazon.com>
2019-06-10 12:06:02 -0700

    stacks and queues done and tests and readmes

---
+++ code-challenges401/src/test/java/code/challenges401/stacksandqueues/QueueTest.java
@@ -0,0 +1,93 @@
+package code.challenges401.stacksandqueues;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueueTest {
+
+    @Test
+    public void test_emptyQueue() {
+        Queue queue = new Queue();
+        assertEquals(null,
+                queue.front);
+    }
+
+    @Test
+    public void test_enqueue_one() {
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        assertEquals(5, queue.front.value);
+        assertEquals(5, queue.back.value);
+    }
+
+    @Test
+    public void test_enqueue_multiple() {
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(-5);
+        assertEquals(5, queue.front.value);
+        assertEquals(-5, queue.back.value);
+    }
+
+    @Test
+    public void dequeue() {
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(-5);
+        assertEquals(5,
+                queue.dequeue());
+        assertEquals(6,
+                queue.front.value);
+        assertEquals(-5,
+                queue.back.value);
+    }
+
+    @Test
+    public void dequeue_toEmpty() {
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(-5);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        assertEquals(null,
+                queue.front);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void test_dequeue_empty(){
+        Queue queue = new Queue();
+        queue.dequeue();
+    }
+
+    @Test
+    public void peek() {
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(-5);
+        assertEquals(5,
+                queue.peek());
+
+    }
+
+
+    @Test (expected = NullPointerException.class)
+    public void test_peek_empty(){
+        Queue queue = new Queue();
+        queue.peek();
+    }
+    @Test
+    public void test_toString() {
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(-5);
+        assertEquals("Queue: Front -> [5] -> [6] -> [-5] -> null",
+                queue.toString());
+    }
+}
