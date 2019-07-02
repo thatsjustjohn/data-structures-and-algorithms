b06df70c28ab9f0496b4d1781236f8d1051ba805
John Winters <jhnwin@amazon.com>
2019-07-02 00:27:21 -0700

    merge sort / readme / code / lecture

--- code-challenges401/src/test/java/code/challenges401/SortTest.java
+++ code-challenges401/src/test/java/code/challenges401/SortTest.java
@@ -31,4 +31,29 @@ public class SortTest {
     public void test_insertionSort_two() {
         assertArrayEquals(new int[] {1,2}, Sort.insertionSort(new int[] {2,1}));
     }
+
+    @Test
+    public void test_mergeSort_multiple() {
+        assertArrayEquals(new int[] {1,2,3,4,5}, Sort.mergeSort(new int[] {5,4,3,2,1}));
+    }
+
+    @Test (expected = NullArgumentException.class)
+    public void test_mergeSort_null() {
+        assertArrayEquals(new int[] {1,2,3,4,5}, Sort.mergeSort(null));
+    }
+
+    @Test
+    public void test_mergeSort_empty() {
+        assertArrayEquals(new int[] {}, Sort.mergeSort(new int[] {}));
+    }
+
+    @Test
+    public void test_mergeSort_one() {
+        assertArrayEquals(new int[] {1}, Sort.mergeSort(new int[] {1}));
+    }
+
+    @Test
+    public void test_mergeSort_two() {
+        assertArrayEquals(new int[] {1,2}, Sort.mergeSort(new int[] {2,1}));
+    }
 }
