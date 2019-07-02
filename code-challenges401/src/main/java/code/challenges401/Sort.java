3cf2267d8370e0a2c331a647b980ef3e462c4c61
John Winters <jhnwin@amazon.com>
2019-07-02 00:41:24 -0700

    add comments

--- code-challenges401/src/main/java/code/challenges401/Sort.java
+++ code-challenges401/src/main/java/code/challenges401/Sort.java
@@ -27,41 +27,48 @@ public class Sort {
         return arr;
     }
 
+    // This function gets called recursively to divide up the array
     public static int[] mergeSort(int[] arr){
         // Cannot call .length on null, this addresses passing in a null object
         if(arr == null) throw new NullArgumentException();
 
         int arrayLength = arr.length;
-
+        // If the length is bigger than 1 divide
         if(arrayLength > 1){
-           int[] leftArr = mergeSort(Arrays.copyOfRange(arr, 0, arrayLength/2));
-           int[] rightArr = mergeSort(Arrays.copyOfRange(arr, arrayLength/2, arrayLength));
-           merge(leftArr, rightArr, arr);
+            // Merge sort on the left half
+            int[] leftArr = mergeSort(Arrays.copyOfRange(arr, 0, arrayLength/2));
+            // Merge sort on the right half
+            int[] rightArr = mergeSort(Arrays.copyOfRange(arr, arrayLength/2, arrayLength));
+            // Merge the 2 half arrays back into original
+            merge(leftArr, rightArr, arr);
         }
 
+        // Return combine array
         return arr;
     }
 
-
+    // Private method to merge 2 arrays into a bigger array in sorted order.
     private static int[] merge(int[] leftArr , int[] rightArr, int[] arr){
+        // Declare indexes
         int i = 0;
         int j = 0;
         int k = 0;
-
+        // While i and j aren't at the end
         while(i < leftArr.length && j < rightArr.length){
+            // If the index of left array is less than the right.. add left to the big array
             if(leftArr[i] <= rightArr[j]){
                 arr[k] = leftArr[i];
                 i++;
-            }else{
+            }else{ // If the index of the right is less than the left.. add right to the big array
                 arr[k] = rightArr[j];
                 j++;
             }
             k++;
-
+            // Finish copying contents of the other array when one of the smaller arrays has finished looping
             if(i == leftArr.length) System.arraycopy(rightArr, j, arr, k, rightArr.length);
             else System.arraycopy(leftArr, i, arr, k, leftArr.length);
         }
-
+        // Return the combine sorted array
         return arr;
     }
 }
