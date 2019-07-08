231f6732ef104b5e96a6474e3e501b8ae9ce3168
John Winters <jhnwin@amazon.com>
2019-07-08 15:02:54 -0700

    hash table and tests

---
+++ code-challenges401/src/main/java/code/challenges401/hashtable/Hashtable.java
@@ -0,0 +1,60 @@
+package code.challenges401.hashtable;
+
+import org.apache.commons.math3.exception.NullArgumentException;
+import org.apache.commons.math3.util.Pair;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+
+class HashNode<K, V>
+{
+    K key;
+    V value;
+    
+    public HashNode(K key, V value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+public class Hashtable<K, V> {
+    LinkedList<HashNode<K, V>>[] hashtable = new LinkedList[1024];
+
+    public void add(String key, V value){
+        int hashedKey = hash(key);
+        if(hashtable[hashedKey] == null) hashtable[hashedKey] = new LinkedList<>();
+        for(HashNode<K,V> hn : hashtable[hashedKey]){
+            if(hn.key.equals(key)) {
+                hn.value = value;
+                return;
+            }
+        }
+        hashtable[hashedKey].add(new HashNode(key, value));
+    }
+
+    public V get(String key){
+        List<HashNode<K,V>> bucket = hashtable[hash(key)];
+        return bucket.stream().filter(item -> item.key.equals(key)).findFirst().get().value;
+    }
+
+    public boolean contains(String key){
+        List<HashNode<K,V>> bucket = hashtable[hash(key)];
+        for(HashNode<K,V> hn : bucket){
+            if(hn.key.equals(key)) return true;
+        }
+        return false;
+    }
+
+    public int hash(String key){
+        if(key == null) throw new NullArgumentException();
+        double hashedKey = 0;
+        int n = key.length();
+        for(int i = 0; i < n; i++){
+            hashedKey += ((int)key.charAt(i) * Math.pow(31, n-(i+1)));
+        }
+        return (int) hashedKey % 1024;
+    }
+}
